09a492d2ad304279478fb70663d0a38c68d15763
ashlee ramos <114506112+ashleeramos@users.noreply.github.com>
2023-01-17 19:06:02 +0000

    hideous

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ var playButton = document.getElementById("play");
 var scoreBox = document.getElementById("score");
 
 function instructions() {
-  let instructs = "<p>best out of how many?</p><input type=\"text\" id=\"howMany\" name=\"games\"><br>";
-  instructs += "enter number of games and press play to start!";
+  let instructs = "<p>best out of how many?</p><input type=\"text\" id=\"howMany\" name=\"games\">";
+  instructs += "<p>enter number of games and press play to start!</p>";
   instruct.innerHTML = instructs;
 }
 
@@ -31,15 +31,14 @@ function displayCD(count) {
   notShoot();
 }
 
-var count = 3;                  //  set your counter to 1
+var count = 3; // set your counter to 1
 
 function countdown() {
-  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
-
-    count--;                    //  increment the counter
-    if (count >= 0) {           //  if the counter < 10, call the loop function
-      myLoop();             //  ..  again which will trigger another 
-    }                       //  ..  setTimeout()
+  setTimeout(function() { // call a 3s setTimeout when the loop is called
+    count--; // increment the counter
+    if (count >= 0) { // if the counter < 10, call the loop function
+      myLoop(); // ...again which will trigger another 
+    } // ...setTimeout()
   }, 500)
 }
 
@@ -51,6 +50,7 @@ function firstRound() {
 function newRound() {
   playButton.setAttribute("onclick", "newRound();");
   scoreBox.innerHTML = roundNum + " out of " + outOf;
+  scoreBox.style.display = "block";
   scoreBox.classList.add("shoot");
   countdown(3);
 }
@@ -58,13 +58,13 @@ function newRound() {
 function countdown(count) {
   screen.innerHTML = "<H1>" + countWords[count] + "</H1>";
   var interval = setInterval(function() {
-    if (count <= 1) clearInterval(interval); //break the interval
+    if (count <= 1) clearInterval(interval); // break the interval
     count--;
     if (count == 0) {
       shoot();
     }
     screen.innerHTML = "<H1>" + countWords[count] + "</H1>";
-  }, 500); //time in milliseconds to wait
+  }, 500); // time in milliseconds to wait
 }
 
 function uTurn(uPlay) {
@@ -73,7 +73,7 @@ function uTurn(uPlay) {
   let text = showRound(cPlay, uPlay) + "<br><br>";
   if (cPlay == uPlay) {
     roundNum--;
-    text += "press play for replay";
+    text += "press play to replay.";
   }
   else {
     text += findWinner(cPlay, uPlay);
@@ -87,14 +87,14 @@ function uTurn(uPlay) {
   }
 }
 
-function main() {
+/* function main() {
   newGame();
   let again = stats();
   if (again == true) {
     newGame();
   }
   else alert("thank you for playing rps!");
-}
+} */
 
 function showRound(cPlay, uPlay) {
   return "i picked " + cPlay + ". you picked " + uPlay + ".";
@@ -132,12 +132,12 @@ function stats() {
   document.getElementById("rpsButtons").style.display = "none";
   let againText = null;
   if (uWins > cWins) {
-    againText = "you won best out of " + outOf;
+    againText = "you won best out of " + outOf + ".";
   }
   else {
     againText = "i won best out of " + cWins + ".";
   }
-  againText += "\npress the play button to play again."
+  againText += "\nthank you for playing! press the play button to play again!"
   screen.innerHTML = againText;
   playButton.value = "play again?";
   playButton.setAttribute("onclick", "location.reload();");
